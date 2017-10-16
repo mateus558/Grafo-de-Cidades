ab83ac9c53dd21bb113ff519e350ffb048614594
rafaelstjf <rafaelstjf@hotmail.com>
2017-10-16 19:06:23 -0200

    gets e sets

--- State.py
+++ State.py
@@ -1,5 +1,23 @@
 class State:
 	
-	def __init__(self):
+	def __init__(self, x = 0.0, y = 0.0, id = " "):
+		self.x = x
+		self.y = y
+		self.id = y
+	def getX:
+		return self.x
+	def getY:
+		return self.y
+	def getId:
+		return self.id
+	def setX(x):
+		self.x = x
+	def setY(y):
+		self.y = y
+	def setId(id):
+		self.id = id
+
+
+	
 	
 	
