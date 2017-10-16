689fb5d66af16c35efc373481fe88d8602209b43
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2017-10-16 20:02:02 -0200

    Reading file being implemented.

--- Solver.py
+++ Solver.py
@@ -1,9 +1,10 @@
 import abc
 from State import *
+from collections import defaultdict
 
 class Solver:
 	
-	def __init__(self, start = State(), end = State(), graph = []):
+	def __init__(self, start = State(), end = State(), graph = {}):
 		self.start = start
 		self.end = end
 		self.graph = graph
@@ -17,8 +18,11 @@ class Solver:
 			line = f.readline()
 			if not line: break
 			res = list(map(float, line.split()))
+			from_state = State(res[0], res[1])
+			to_state = State(res[2], res[3])
+			self.graph[from_state].append(to_state)
 			
-		
+			print(self.graph.items())
 		return self.graph
 
 g = Solver()
--- State.py
+++ State.py
@@ -3,17 +3,24 @@ class State:
 	def __init__(self, x = 0.0, y = 0.0, id = " "):
 		self.id = y
 		self.pos = [x, y]
+	
 	def getPos(self):
 		return self.pos
+	
 	def getId(self):
 		return self.id
+	
 	def setPos(self,x, y):
 		self.pos = [x, y]
-	def setId(self, id)
+	
+	def setId(self, id):
 		self.id = id
+		
 	def __hash__(self):
-		return hash((self.x, self.y))
+		return hash((self.pos[0], self.pos[1]))
+	
 	def __eq__(self, other):
 		return (self.x, self.y) == (other.x, other.y)
+	
 	def __ne__(self, other):
 		return not(self == other)
