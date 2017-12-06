4f968f7cc7dedd722dc33bb37683f4a5d90c8f73
rafaelstjf <rafaelstjf@hotmail.com>
2017-12-06 21:12:31 -0200

    mudancas no backtracking

--- Backtracking.py
+++ Backtracking.py
@@ -14,37 +14,25 @@ class Backtracking(Solver):
         state = self.start
         iterations = 0
         start_time = time.time()
-
-        while(not failure or not sucess):
-            #enquanto nao conseguir sucesso ou fracasso ele vai pegar o "operador" do objeto
-            #preciso pegar um determinado elemento da lista
-            if(state.getOperator() < len(self.graph[state])):
-                rn = self.graph[state][state.getOperator()][0]
-                rn.increaseCostSoFar(1)	
-                state.setOperator(state.getOperator() + 1)
-                self.visited.append(rn)
-                rn.setFather(state)
-                state = rn
-                if(state == self.end):
-                    sucess = True
-            else:
-                if(state == self.start):
-                    failure = True
-                else:
-                    state = state.getFather()
-                iterations = iterations + 1
+        sol = solveAux(state, self.end, iterarions)
         end_time = time.time()
-        if(sucess):
-            itr = state
-            cost = itr.getCost()
-            path = []
-            time_elapsed = end_time - start_time
-            while itr != self.start:
-                path.append(itr)
-                itr = itr.getFather()            
-            path.append(self.start)
-            path.reverse()
-            self.solution = Solution(path, cost, 0, 0, self.visited, time_elapsed)
+        s = sol
+        while(s is not None):
+            self.visited.append(s)
+            s = s.getFather()
+        if(sol is not None):
+            return self.setSolution(self.end, iterations, 0, 0, self.visited, time_elapsed)
+
+    def solveAux(state, end, iterations):
+        iterations = iterations + 1
+        if(state == end):
+            return state;
+        elif(len(self.graph[state]) > 0):
+            for s in self.graph[state]:
+                s.setFather(state)
+                s.setDepth(state.getDepth() + 1)
+                n = solveAux(s)
+                if(n is not None):
+                    return n
         else:
-            self.solution is None
-        return self.solution
+            return None
