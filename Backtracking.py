8fb823d9299e67bfee7b844df5ffa8176acb249d
Rafael <rafaelstjf@hotmail.com>
2017-12-07 13:40:58 -0200

    Backtracking pronto

--- Backtracking.py
+++ Backtracking.py
@@ -7,44 +7,37 @@ class Backtracking(Solver):
     def __init__(self, start=State(), end=State(), graph=defaultdict(list)):
         super(Backtracking, self).__init__(start, end, graph=defaultdict(list))
         self.visited = []
+        self.iteration = 0
+        self.branchingsums = 0
+
+    def solveAux(self, state):
+        print(state)
+        self.visited.append(state)
+        self.iterations = self.iterations +1
+        if(state == self.end):
+            self.end = state
+            return state
+        elif(len(self.graph[state]) > 0):
+            for i in range(0,len(self.graph[state])):
+                self.branchingsums = self.branchingsums + 1
+                self.graph[state][i][0].setFather(state)
+                self.graph[state][i][0].setDepth(state.getDepth() + 1)
+                st = self.solveAux(self.graph[state][i][0])
+                if(st is not None):
+                    return st
+            return None
+        else:
+            return None
     def solve(self):
         #inicia sem sucesso e sem fracasso
         sucess = False
         failure = False
         state = self.start
-        state.setFather(None)
-        state.setDepth(0)
-        iterations = 0
+        self.iterations = 0
         start_time = time.time()
-        while(not failure or not sucess):
-            #enquanto nao conseguir sucesso ou fracasso ele vai pegar o "operador" do objeto
-            #preciso pegar um determinado elemento da lista
-            iterations = iterations + 1
-            if(state == self.start):
-                print(state)
-                print(state.getOperator())
-            if(state.getOperator() < len(self.graph[state])):
-                rn = self.graph[state][state.getOperator()][0]
-                for a in self.visited:
-                    if(rn == a):
-                        if(rn.getFather() == rn.getFather):
-                            rn.increaseCostSoFar(1)	
-                            rn.setDepth(state.getDepth() +1)
-                            if(rn not in self.visited):
-                                self.visited.append(rn)
-                                rn.setFather(state)
-                                state = rn
-                state.setOperator(state.getOperator() + 1)
-                if(state == self.end):
-                    sucess = True
-            else:
-                if(state == self.start):
-                    failure = True
-                else:
-                    state = state.getFather()
+        sol = self.solveAux(self.start)
         end_time = time.time()
         time_elapsed = end_time - start_time
-        if(sucess):
-            return self.setSolution(state, iterations, 0,0, self.visited, time_elapsed)
-        else:
-            self.solution is None
+        return self.setSolution(self.end, self.iterations, branchingSum/iterations, 0, self.visited, time_elapsed)
+
+   
